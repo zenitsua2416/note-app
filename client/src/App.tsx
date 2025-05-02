f544c376d08fcaf6a252f6a7bb6a4ab380e9a904
Zenitsu Agatsuma <zenitusa2416@gmail.com>
2025-05-02 16:58:41 +0530

    feat: setup react router
    what's changed:
    - Add router with createBrowserRouter
    - Add Home Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,7 @@
-import { useState } from "react";
+import { RouterProvider } from "react-router-dom";
 
-import { DefaultLayout } from "@/layouts";
-import { useAppSelector } from "@/hook";
-import { selectTheme } from "@/features";
+import { router } from "@/routes";
 
-const App = () => {
-  const [count, setCount] = useState<number>(0);
-  const { theme } = useAppSelector(selectTheme);
-
-  return (
-    <main className={theme === "dark" ? "dark" : ""}>
-      <DefaultLayout>
-        <div className="flex h-screen w-screen flex-col items-center justify-center dark:bg-gray-900 dark:text-gray-100">
-          <h1 className="my-10 text-3xl font-bold">My Simple Vite App⭐</h1>
-          <button
-            onClick={() => setCount((prev) => prev + 1)}
-            className="rounded-md border bg-gray-200 p-2 px-4 font-semibold outline-none hover:bg-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-800"
-          >
-            Count is {count}
-          </button>
-        </div>
-      </DefaultLayout>
-    </main>
-  );
-};
+const App = () => <RouterProvider router={router} />;
 
 export default App;
---
+++ client/src/pages/Home/HomePage.tsx
@@ -0,0 +1,7 @@
+export const HomePage = () => (
+  <div>
+    <h1 className="text-center text-3xl font-semibold text-blue-500">
+      Home Page
+    </h1>
+  </div>
+);
