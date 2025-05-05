5b637b9aa6a3f14edef65d683697baa3bfeca699
Zenitsu Agatsuma <zenitusa2416@gmail.com>
2025-05-05 19:12:30 +0530

    chore(build): Update vite config
    what's changed:
    - Update vite build config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,28 +1,109 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["./src/vitest.setup.ts"],
-    css: true,
-    testTimeout: 5000,
-    reporters: ["verbose"],
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html", "lcov"],
-      reportsDirectory: "./coverage",
+import type { UserConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+/* Common Config for both PROD and DEV mode */
+const commonConfig: UserConfig = {
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    viteCompression(),
+    visualizer({
+      brotliSize: true,
+      gzipSize: true,
+      open: true,
+      sourcemap: false,
+      filename: "dist/stats.html",
+    }),
+  ],
+  /* Customizing build folder structure */
+  build: {
+    /* 
+        Imported or referenced assets that are smaller than 4KiB threshold will be inlined as base64 URLs to avoid extra http requests.
+        Set to 0 to disable inlining altogether
+        */
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        entryFileNames: "js/[name]-[hash].js",
+        assetFileNames: ({ name }) => {
+          if (/\.(webp|jpe?g|png)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+          if (/\.(woff2|ttf)$/.test(name ?? ""))
+            return "assets/fonts/[name]-[hash][extname]";
+          return "[name]-[hash][extname]";
+        },
+      },
     },
   },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+};
+
+export default defineConfig(({ mode }): UserConfig => {
+  /* Load environment variables based on the mode */
+  const env = loadEnv(mode, process.cwd(), "");
+  const PORT = parseInt(env.PORT) || undefined;
+
+  /* Production-specific configuration */
+  if (mode === "production") {
+    return {
+      ...commonConfig,
+      plugins: [
+        commonConfig.plugins,
+        /* Image optimization for production build */
+        ViteImageOptimizer({
+          test: /\.(jpe?g|webp|png)$/i,
+          includePublic: false,
+          logStats: true,
+          jpg: {
+            quality: 90,
+          },
+          jpeg: {
+            quality: 90,
+          },
+          webp: {
+            quality: 90,
+          },
+          png: {
+            quality: 90,
+          },
+        }),
+      ],
+      build: {
+        ...commonConfig.build,
+        sourcemap: "hidden", // Do not expose sourcemaps
+        minify: "terser", // Terser for minification
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      },
+      server: {
+        strictPort: true,
+        port: PORT,
+      },
+    };
+    /* Development-specific configuration */
+  } else if (mode === "development") {
+    return {
+      ...commonConfig,
+      build: {
+        ...commonConfig.build,
+        sourcemap: "inline", // Include inline sourcemaps for easier debugging
+      },
+      server: {
+        strictPort: true,
+        port: PORT,
+      },
+    };
+  } else return commonConfig;
 });
