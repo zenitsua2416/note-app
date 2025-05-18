ec9b2817edff9f93a8f07f5a93919878be9c3272
Zenitsu Agatsuma <169963839+zenitsua2416@users.noreply.github.com>
2025-05-18 17:29:25 +0530

    style: Ensure responsiveness for the Home Page

--- client/src/components/ui/NoteGrid/NoteGrid.tsx
+++ client/src/components/ui/NoteGrid/NoteGrid.tsx
@@ -4,7 +4,7 @@ import { Each } from "@/components/utils";
 import { NoteGridProps } from "./NoteGrid.types";
 
 export const NoteGrid = ({ notes, endChild }: NoteGridProps) => (
-  <div className="grid grid-cols-3 items-stretch gap-x-4 gap-y-6">
+  <div className="grid grid-cols-1 items-stretch gap-x-4 gap-y-6 sm:grid-cols-2 lg:grid-cols-3">
     <Each of={notes} render={(note) => <Note key={note.id} {...note} />} />
     {endChild}
   </div>
