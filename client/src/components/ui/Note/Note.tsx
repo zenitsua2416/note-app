e9d3b973052803119e0c2fb4d6ef992483a1e04f
Zenitsu Agatsuma <169963839+zenitsua2416@users.noreply.github.com>
2025-05-29 23:11:57 +0530

    fix(ui/notegrid): Fix `NoteGrid`, with broken gapping
    what's changed:
    - Fix `NoteGrid` broken gapping
    - Consistent `Note` item styles in `NoteGrid`

--- client/src/components/ui/Note/Note.tsx
+++ client/src/components/ui/Note/Note.tsx
@@ -11,7 +11,7 @@ import { Note as NoteProps } from "@/types";
  * @component
  */
 export const Note = ({ id, title, notes: content }: NoteProps) => (
-  <div className="bg-default-200/50 hover:bg-default-200/75 relative inline-block cursor-pointer rounded-lg py-4">
+  <div className="bg-default-200/50 hover:bg-default-200/75 relative inline-block cursor-pointer self-stretch rounded-lg py-4">
     {/* Card Header */}
     <div className="flex items-center px-4">
       <Link to={NOTE_ROUTE(id)}>
--- client/src/components/ui/NoteGrid/NoteGrid.tsx
+++ client/src/components/ui/NoteGrid/NoteGrid.tsx
@@ -4,7 +4,7 @@ import { Each } from "@/components/utils";
 import { NoteGridProps } from "./NoteGrid.types";
 
 export const NoteGrid = ({ notes, endChild }: NoteGridProps) => (
-  <div className="grid grid-cols-1 items-stretch gap-x-4 gap-y-6 sm:grid-cols-2 lg:grid-cols-3">
+  <div className="grid auto-rows-fr grid-cols-1 items-stretch gap-x-4 gap-y-6 sm:grid-cols-2 lg:grid-cols-3">
     <Each of={notes} render={(note) => <Note key={note.id} {...note} />} />
     {endChild}
   </div>
