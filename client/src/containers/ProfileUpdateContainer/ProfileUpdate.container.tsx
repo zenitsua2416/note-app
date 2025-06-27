31c3965b6aaa055fd13ac787fab1a039874f655c
Zenitsu Agatsuma <169963839+zenitsua2416@users.noreply.github.com>
2025-06-28 00:46:50 +0530

    feat: Correctly update the display name in the supabase

--- client/src/containers/ProfileUpdateContainer/ProfileUpdate.container.tsx
+++ client/src/containers/ProfileUpdateContainer/ProfileUpdate.container.tsx
@@ -1,38 +1,109 @@
+import { useCallback, useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 
-import { Button, Input } from "@heroui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Input, Spinner } from "@heroui/react";
+import { z } from "zod";
 
 import { selectUserProfile } from "@/features";
-import { useAppSelector } from "@/hooks";
+import { useAppSelector, useNotify } from "@/hooks";
+import { supabase } from "@/supabase";
+
+const ProfileUpdateSchema = z.object({
+  displayName: z.string().min(3),
+});
+
+type ProfileUpdateSchema = z.infer<typeof ProfileUpdateSchema>;
 
 export const ProfileUpdateContainer = () => {
+  const notify = useNotify();
   const userProfile = useAppSelector(selectUserProfile);
-  const { register } = useForm({
-    defaultValues: {
-      displayName: userProfile?.full_name,
-    },
+  const {
+    handleSubmit,
+    register,
+    reset,
+    watch,
+    formState: { isSubmitting },
+  } = useForm<ProfileUpdateSchema>({
+    mode: "onTouched",
+    resolver: zodResolver(ProfileUpdateSchema),
   });
+
+  useEffect(() => {
+    if (userProfile.full_name) {
+      reset({
+        displayName: userProfile.full_name,
+      });
+    }
+  }, [userProfile, reset]);
+
+  // Refs
+  const initialDisplayName = useRef(userProfile?.full_name ?? "");
+
+  const onSubmit = useCallback(
+    async (data: ProfileUpdateSchema) => {
+      const { error, data: res } = await supabase
+        .from("user_profile")
+        .update({
+          full_name: data.displayName,
+        })
+        .eq("id", userProfile.id);
+
+      if (error) {
+        console.log(error);
+        notify({
+          title: "Cannot update profile",
+          description: "Please try again later",
+          type: "error",
+        });
+        return;
+      }
+
+      console.log(res);
+      notify({
+        title: "Profile updated",
+        description: "Your profile has been updated",
+        type: "success",
+      });
+      initialDisplayName.current = data.displayName;
+    },
+    [notify, userProfile.id],
+  );
+
+  if (userProfile.user_id === "") {
+    return (
+      <div className="mb-8 flex gap-2">
+        <Spinner variant="simple" size="sm" color="current" />
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  const currentDisplayName = watch("displayName");
+
+  const isChanged = currentDisplayName !== initialDisplayName.current;
 
   return (
     <>
-      <h2 className="my-12 text-2xl font-semibold">Your Profile</h2>
-      <form className="mb-8 flex w-full max-w-96 flex-col items-start gap-4">
+      <h2 className="mb-12 text-2xl font-semibold">Your Profile</h2>
+      <form
+        className="mb-8 flex w-full max-w-96 flex-col items-start gap-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <Input
           id="displayName"
           label="Display Name"
           labelPlacement="outside"
           placeholder="John Doe..."
-          autoComplete="off"
-          {...register("displayName", {
-            required: "Display name is required",
-          })}
+          {...register("displayName")}
         />
         <Button
           type="submit"
           color="primary"
-          // disabled={loading}
+          isLoading={isSubmitting}
+          isDisabled={!isChanged}
         >
-          Save
+          {isSubmitting ? "Updating..." : "Update"}
         </Button>
       </form>
     </>
