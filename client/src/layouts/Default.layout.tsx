d8b9464c3e13561907810e0f3f26a79737d435da
Zenitsu Agatsuma <169963839+zenitsua2416@users.noreply.github.com>
2025-06-28 01:15:24 +0530

    fix: Fix avatar menu theme color & font family
    what's changed:
    - The avatar menu is always dark themed until now & is of normal defualt
      ubuntu font
    - Fix these issues and improve UI, with consistent theme & font

--- client/src/layouts/Default.layout.tsx
+++ client/src/layouts/Default.layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import { NavBarContainer } from "@/containers";
 
 export const DefaultLayout = () => (
-  <div className="font-inter bg-default-50 text-default-800 grid min-h-screen grid-cols-[minmax(0,1fr)] grid-rows-[auto_1fr_auto]">
+  <div className="bg-default-50 text-default-800 grid min-h-screen grid-cols-[minmax(0,1fr)] grid-rows-[auto_1fr_auto]">
     <NavBarContainer />
 
     <main>
